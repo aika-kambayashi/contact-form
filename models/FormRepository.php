af8ab6acd66095826002cd6a16798916c95f4795
Aika Kambayashi <kambayashi.aika@no1s.biz>
2021-08-02 11:51:36 +0900

    commit to 'master'

--- models/FormRepository.php
+++ models/FormRepository.php
@@ -11,6 +11,7 @@ class FormRepository extends DbRepository {
             'address1'      => '',
             'address2'      => '',
             'comment'       => '',
+            'mail_address'  => '',
         ];
         return $data;
     }
@@ -18,7 +19,7 @@ class FormRepository extends DbRepository {
     public function insert($form) {
         //プリペアステートメント用のSQL作成
         $sql = "
-            insert into form(name,age,prefecture,address1,address2,comment) values(:name,:age,:prefecture,:address1,:address2,:comment)
+            insert into form(name,age,prefecture,address1,address2,comment,mail_address) values(:name,:age,:prefecture,:address1,:address2,:comment,:mail_address)
         ";
         //SQL実行
         $stmt = $this->execute($sql,$form);
